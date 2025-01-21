5e11d8ce47461c772433d17dc6bb47e0aff22cc0
PRI <priyangshu.mandal1055@gmail.com>
2025-01-21 12:45:42 +0530

    Create Example3.py

---
+++ Day 4/Video18/Example3.py
@@ -0,0 +1,11 @@
+i=int(input("Enter the number"))
+print(i)
+while(i<=38):
+    i=int(input("Enter the number"))
+    print(i)
+print("Done with loops")
+
+
+
+
+#similer to do-while loopssssssssssssssssssss........................
