a6cd9bd7acfb5b564219089fc0ca27638424a332
PRI <priyangshu.mandal1055@gmail.com>
2025-01-21 22:51:35 +0530

    Create Example2.py

---
+++ Day 4/Video20/Example2.py
@@ -0,0 +1,10 @@
+def calculate(a,b):
+    mean=(a+b/2)
+    print(mean)
+def lessthan(a,b):
+    pass                  #We can write the function latter
+
+
+a=10
+b=15                    #Hardcode
+calculate(a,b)
