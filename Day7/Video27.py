4110b36a13017cc72939826ce57d96460aa94bb9
PRI <priyangshu.mandal1055@gmail.com>
2025-01-30 09:58:41 +0530

    Create Video27.py

---
+++ Day7/Video27.py
@@ -0,0 +1,13 @@
+#create  a program to  capable of display questions to the user like kbc
+
+question1=print("Capital of india \n1:Kolkata\n2:Dilhi\n3:Maharast\n4:Mumbai")
+c=int(input("Enter ans Between(1-4):"))
+if (c==2):
+    print("CORRECT  ANS, YOU OWN 1000 RS")
+    print("The secound question on computer screen...")
+    question2=print("Capital of Westbengal is: \n1:Kolkata\n2:Dilhi\n3:Maharast\n4:Mumbai")
+    d=int(input("Enter ans Between(1-4):"))
+    if (d==1):
+        print("CORRECT  ANS, YOU OWN 10000 RS")
+       #2
+       # print("The secound question on computer screen...")
